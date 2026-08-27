690a3552e07aa23b65925d069908661b5fcef043
maitrichavan30 <maitrichavan30@gmail.com>
2026-08-27 13:47:56 +0000

    Add variables and data types lessons

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") // we are using node js not browser
+
+//console.log(3 + 3) console.log(3 - 3) //not good 
+
+console.log(3 + 3) //code readability is very important
+console.log(3 - 3)
+
+//dATA tyPE
+let name = "maitri" //string
+let age = 19 //number
+let isLoggedIn = false //boolean
+
+// number => 2 to the power of 53
+// bigint => larger than 2 to the power of 53
+// string => any value within quotes and prefer in "" instead of '' or `` of '' 
+// boolean => true or false
+// null => empty value/standalone value
+// undefined => value not assigned
+// symbol => unique identifier
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "maitrichavan30.com"
+var accountPassword = "123456"
+accountCity = "Pune"
+let accountState 
+
+accountEmail = "mai@gmail.com"
+accountPassword = "12345678"
+accountCity = "Mumbai"
+
+// accountId = 1000
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block scope and function scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
