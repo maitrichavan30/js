792ac13b03e9488e330728711b0660ea3caffb3f
maitrichavan30 <maitrichavan30@gmail.com>
2026-08-30 13:18:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33c4"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueIntNumber = Number(score)
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// undefined => NaN
+
+let isLoggedIn = 0 
+
+let booleanIsLoggin = Boolean(isLoggedIn);
+console.log(booleanIsLoggin);
+
+//1 => true; 0 => false
+// "" => false;
+// "maitri" => true
+
+let somestring = 33;
+
+let stringValue = String(somestring);
+console.log(stringValue);
+console.log(typeof stringValue);
