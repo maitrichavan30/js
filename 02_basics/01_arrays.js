131bf24c6e137750b7976b3bdaa9dd56f0413084
maitrichavan30 <maitrichavan30@gmail.com>
2026-09-03 14:24:12 +0000

    Add arrays basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,36 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeors = ["Iron Man", "Spider Man", "Thor", "Hulk", "Captain America"];
+
+const myArr2 = new Array(1, 2, 3, 4, 5);
+console.log(myArr[1]);
+
+// Array methods
+
+myArr.push(6);
+myArr.push(7);
+myArr.pop();
+
+myArr.unshift(8);
+myArr.shift();
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+
+const newArr = myArr.join();
+console.log(typeof newArr);
+
+//slice,splice
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3);
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log("C ", myArr);// this is the main difference array which is changed after splice
+console.log(myn2);
+
+
