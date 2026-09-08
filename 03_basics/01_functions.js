9479f8e26d065fcf3074ae95d670aa735950b767
maitrichavan30 <maitrichavan30@gmail.com>
2026-09-08 16:39:53 +0000

    Update function examples

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,7 +30,7 @@ addTwoNumbers(3, 4); // this will call the function and execute the code inside 
 //addTwoNumbers(3,"4") 
 //addTwoNumbers(3,"a")
 //addTwoNumbers(3,null) // like these function gives the output as per the data type of the parameters passed in the function
-                         (//username = "maitri")
+
 function loginUserMessage(username){
     if(username === undefined){
         console.log("Please enter a username");
@@ -44,4 +44,37 @@ function loginUserMessage(username){
     return `${username} just logged in`; 
 }
 
-console.log(loginUserMessage("maitri"));
+//console.log(loginUserMessage("maitri"));
+
+function calculateCartPrice(val1,val2, ...num1) { // ... this 3 dot  called rest operator and the spread operator is used to spread the array into individual elements
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500,2000)); 
+// when you put val1 , val2 then in the num1 comes only[500,2000] because in val1 [200] and in val2 [400]
+
+// const user = {
+//     username: "maitri",
+//     price: 199
+// }
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);    
+}
+
+//handleObject(user)
+handleObject ({
+    username: "maiitri",
+    price:899
+})
+
+//passing array now
+
+const myNewArray = [200,400,100,600] 
+
+function returnSecondValue(getArray){
+    return getArray[0]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
