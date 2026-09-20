b741c8d540ee253bc0865e9cae40a14ef74cef3b
maitrichavan30 <maitrichavan30@gmail.com>
2026-09-20 17:20:09 +0000

    Add do while loop exercise

---
+++ 05_iterations/dowhileloop.js
@@ -0,0 +1,30 @@
+// while loop
+let index = 0;
+
+while (index <= 10) {
+    console.log(`value  of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(`value of ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+
+// Do while loop
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 10);
+
+let score1 = 11
+do { // here the code is run one time then stop the condition
+    console.log(`score is ${score}`);
+    score1++;
+} while (score <= 10);
