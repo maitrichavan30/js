fde41ddd758f3a32cba51fe7ce4711f7f9d87359
maitrichavan30 <maitrichavan30@gmail.com>
2026-09-25 09:20:22 +0000

    Add filter and reduce examples

---
+++ 05_iterations/filtermapreduce.js
@@ -0,0 +1,53 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     //return item
+// })
+
+//console.log(values); // gives undefined because foreach doesn't return anything
+
+// filter
+const myNums = [1,2,3,4,5,5,6,7,8,9,10]
+// 1st method
+// const newNums = myNums.filter( (nums) => nums > 4)
+// console.log(newNums);
+
+// 2nd methos
+// const newNums = myNums.filter( (nums) => {
+//     return nums > 4 // when you open scope for callback then you should return 
+// })
+// console.log(newNums);
+
+// 3rd method using forEachloop
+const newNums =  []
+myNums.forEach ( (num) => {
+    if (num > 4){
+        newNums.push(num)
+    }
+})
+
+console.log(newNums);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ]; 
+
+  let userBook = books.filter ( (bk) => bk.genre === 'history')
+
+  userBook = books.filter ( (bk) => {
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+//here if we didn't write return gets error why because in upper one we write condition  without scope{} 
+// for solve this error if you write without scope or make return 
+  console.log(userBook);
+  
